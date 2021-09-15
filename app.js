e183ad0eb22f3645761685dde4f322ca6d248609
Ernesto-Ordonez <50756055+Ernesto-Ordonez@users.noreply.github.com>
2021-09-15 15:44:33 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+// Defining functions
+
+function grumpus() {
+    console.log("ugh");
+    console.log("not this again");
+    console.log("just 5 mins more...");
+}
+
+function singSong() {
+    console.log("DO");
+    console.log("RE");
+    console.log("MI");
+}
+
+function greet(firstName) {
+    console.log(`Hey there, ${firstName}`);
+}
+
+// Running functions
+greet("Carlos");
